fa1b8b69e42a6e0da8522415093148f351465108
Christian Anderson <somecallmehans@gmail.com>
2022-02-02 14:39:29 -0500

    Map set up

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Map from "./Map";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is the home screen!</h1>
+      <Map />
+    </div>
+  );
+}
---
+++ src/components/Map.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -1.2884,
+  lng: 36.8233,
+};
+
+export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {} <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
